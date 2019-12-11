8df41b4846d35f5dc4eed6f5e9220bbd92b67810
Tim Anema <tim.anema@shopify.com>
2019-12-11 16:10:32 -0500

    blob

--- term/screenbuf.go
+++ term/screenbuf.go
@@ -44,7 +44,7 @@ func NewScreenBuf(w io.Writer) *ScreenBuf {
 
 func (s *ScreenBuf) reset() {
 	linecount := bytes.Count(s.buf.Bytes(), []byte("\n"))
-	reset := strings.Repeat("\033[1F\033[2K", linecount)
+	reset := strings.Repeat("\033[0G\033[1A\033[0K", linecount)
 	s.buf.Reset()
 	s.buf.Write([]byte(reset))
 }
